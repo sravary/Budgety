23ccc537a853541124e73096ca3e2346387f2ed1
sravary <sravary@hotmail.com>
2020-04-13 22:05:29 -0400

    add eventListener for click + enter key

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function() {
-        
+    var ctrlAddItem = function() {
         // 1. Get the field input data
 
         // 2. Add item to the budget controller
@@ -26,6 +25,21 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
+
+        console.log('It will work properly soon enough!')
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    // This also adds a keypress event listener, specifically the return/enter key
+    // We will need its keyCode number by console logging the event in the DOM
+    // NOTE: event.which is for older browsers who do not recognize keyCodes
+    document.addEventListener('keypress', function(event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
     });
 
     
