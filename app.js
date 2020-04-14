1da5ede66c1b338f8c1bd5692aaa9b3765a95cf1
sravary <sravary@hotmail.com>
2020-04-14 00:01:00 -0400

    select type/descr/value

--- app.js
+++ app.js
@@ -8,6 +8,29 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
+    // Create object for all the class selectors
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    // This is the method of returning all three inputs from the interface
+    return {
+        getinput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // will either be inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        // This will make the private DOMstrings public for us to use below in the Global App Controller
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+
 })();
 
 
@@ -15,8 +38,12 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM =  UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
         // 1. Get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
 
         // 2. Add item to the budget controller
 
@@ -25,11 +52,9 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-
-        console.log('It will work properly soon enough!')
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     // This also adds a keypress event listener, specifically the return/enter key
     // We will need its keyCode number by console logging the event in the DOM
