e397c3d322cd7a97a2d45d6cffef8ad138182e6d
sravary <sravary@hotmail.com>
2020-06-20 23:38:40 -0400

    displaying correct %

--- app.js
+++ app.js
@@ -162,7 +162,8 @@ var UIController = (function() {
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
     }
 
     // This is the method of returning all three inputs from the interface
@@ -235,6 +236,26 @@ var UIController = (function() {
             }
         },
 
+        displayPercentages: function(percentages) {
+
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+            var nodeListForEach = function(list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fields, function(current, index) {
+
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%'; 
+                } else {
+                    current.textContent = '---';
+                }
+            });
+        },
+
         // This will make the private DOMstrings public for us to use below in the Global App Controller
         getDOMstrings: function() {
             return DOMstrings;
@@ -282,7 +303,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 2. Read percentages from the budget controller
         var percentages = budgetCtrl.getPercentages();
         // 3. Update the UI with the new percentages
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
     };
 
     var ctrlAddItem = function() {
